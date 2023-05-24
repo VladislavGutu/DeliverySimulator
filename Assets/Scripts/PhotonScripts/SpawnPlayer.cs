fd9b1185d89715d855b1880052f229bfb46e5205
Vlad <vladik.gutu@gmail.com>
2023-05-24 23:15:19 +0300

    photon

---
+++ Assets/Scripts/PhotonScripts/SpawnPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public GameObject _playerPrefab;
+    public GameObject _spawnPoint;
+    
+    void Start()
+    {
+        Vector3 randomPos = new Vector3(_spawnPoint.transform.position.x + Random.Range(-3, 4), _spawnPoint.transform.position.y, _spawnPoint.transform.position.z);
+        PhotonNetwork.Instantiate(_playerPrefab.name, randomPos, Quaternion.identity);
+    }
+}
